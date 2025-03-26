b43edda9d8072daee693e1b796c2e43c6a99f1e7
Rinor <rinorsahiti@hennessey.com>
2025-03-26 20:55:09 +0100

    push

---
+++ frontpage.php
@@ -0,0 +1,13 @@
+
+<?php /* Template Name: Home */ 
+include("includes/headers/rightmenu.php"); 
+?>
+
+<main id="primary" class="site-main">
+
+<?php get_template_part('includes/blocks/block-banner-one', null, array()); ?>
+
+
+</main>
+
+<?php include("includes/footers/default.php");  ?>
