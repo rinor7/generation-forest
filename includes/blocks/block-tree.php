fe0e2d23d11b6d08ef0575e31183e929a3ac4925
Rinor <rinorsahiti@hennessey.com>
2025-04-03 01:09:52 +0200

    push

--- includes/blocks/block-tree.php
+++ includes/blocks/block-tree.php
@@ -4,22 +4,22 @@
         <div class="section-header">
             <?php 
             // Fetch the entire 'three_box-default' group
-            $three_box_values = get_field('three_box-default');
+            $tree_values = get_field('tree');
             
             // Check for the section title and subtitle
-            $has_title = !empty($three_box_values['section_title']);
-            $has_subtitle = !empty($three_box_values['section_subtitle']);
+            $has_title = !empty($tree_values['section_title']);
+            $has_subtitle = !empty($tree_values['section_subtitle']);
             ?>
             
             <?php if ($has_title): ?>
                 <h2 class="<?php echo (!$has_subtitle ? 'only-title' : ''); ?>">
-                    <?php echo esc_html($three_box_values['section_title']); ?>
+                    <?php echo esc_html($tree_values['section_title']); ?>
                 </h2>
             <?php endif; ?>
             
             <?php if ($has_subtitle): ?>
                 <h3 class="<?php echo (!$has_title ? 'only-subtitle' : ''); ?>">
-                    <?php echo esc_html($three_box_values['section_subtitle']); ?>
+                    <?php echo esc_html($tree_values['section_subtitle']); ?>
                 </h3>
             <?php endif; ?>
         </div>
@@ -37,7 +37,19 @@
                     <img src="<?php echo esc_url($background_image['url']); ?>" alt="Tree Background">
                 </div>
             <?php endif; ?>
+
+            <?php 
+            $tree_text = $tree_values['tree_text'] ?? '';
+            $enable_desktop = $tree_values['enable_tree_text_desktop'] ?? false;
+            $enable_mobile = $tree_values['enable_tree_text_mobile'] ?? false;
+
+            if ($tree_text && ((!wp_is_mobile() && $enable_desktop) || (wp_is_mobile() && $enable_mobile))): ?>
+                <div class="tree-text">
+                    <?php echo esc_html($tree_text); ?>
+                </div>
+            <?php endif; ?>
         </div>
+
 
     </div>
 </section>
