e121047bf94f11e1febd358736523c7ddc872be0
Rinor <rinorsahiti@hennessey.com>
2025-04-04 10:49:17 +0200

    push

--- theme-options/post-types.php
+++ theme-options/post-types.php
@@ -1,24 +1,24 @@
 <?php 
 
 function custom_post_type() {
-    register_post_type('posttype', array(
-        'labels' => array('name' => 'Post Type'),
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions'),
-        'taxonomies' => array('custom_taxonomy', 'post_tag'), //if you need "Uncategorized" category, replace "custom_taxonomy" with "category"
-        'menu_position' => 8,
-        'menu_icon' => 'dashicons-welcome-add-page',
-    ));
-
-    // register_post_type('our-projects', array(
-    //     'labels' => array('name' => 'Our Projects'),
+    // register_post_type('posttype', array(
+    //     'labels' => array('name' => 'Post Type'),
     //     'public' => true,
     //     'has_archive' => true,
     //     'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions'),
-    //     'taxonomies' => array('category', 'post_tag'), //if you need "Uncategorized" category, replace "custom_taxonomy" with "category"
+    //     'taxonomies' => array('custom_taxonomy', 'post_tag'), //if you need "Uncategorized" category, replace "custom_taxonomy" with "category"
     //     'menu_position' => 8,
     //     'menu_icon' => 'dashicons-welcome-add-page',
     // ));
+
+    register_post_type('our-projects', array(
+        'labels' => array('name' => 'Our Projects'),
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions'),
+        'taxonomies' => array('category', 'post_tag'), //if you need "Uncategorized" category, replace "custom_taxonomy" with "category"
+        'menu_position' => 8,
+        'menu_icon' => 'dashicons-welcome-add-page',
+    ));
 }
 add_action('init', 'custom_post_type');
