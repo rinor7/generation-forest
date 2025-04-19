c7a184a88117980dd5e5cb87d0b27cf50428c2da
Rinor <rinorsahiti@hennessey.com>
2025-04-19 18:38:20 +0200

    repush

--- includes/headers/default.php
+++ includes/headers/default.php
@@ -7,6 +7,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <meta name="theme-color" content="red">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
